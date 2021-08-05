72817b434e6cc149cee42fbe27e3b58aae39e783
hanWang729 <13240928761@163.com>
2021-08-05 10:27:47 +0000

    Task 0 and 1 complete

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,7 +118,9 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    _chatLogic = std::unique_ptr<ChatLogic> (new ChatLogic());
+    
+    
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -135,7 +137,8 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    // delete _chatLogic;
+    //std:: << "CharLogic destroyed\n";
 
     ////
     //// EOF STUDENT CODE
